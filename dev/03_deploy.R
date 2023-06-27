b3a97c126d8ebab665971f2ac1cad783dfd84b8a
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2023-06-27 13:54:23 +0300

    local documentation

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -8,15 +8,17 @@
 ## Check the package before sending to prod
 devtools::check()
 
-#
+# # pre build vignette localy
 # options(rmarkdown.html_vignette.check_title = FALSE)
-# knitr::knit("vignettes/tutorial_add_info.Rmd.orig", output = "vignettes/tutorial_add_info.Rmd")
-# devtools::build_rmd("vignettes/tutorial_add_info.Rmd")
-# browseURL("vignettes/tutorial_add_info.html")
-
-
-# increase version
+# knitr::knit("vignettes/run_on_demo_repo.Rmd.orig", output = "vignettes/run_on_demo_repo.Rmd")
+# devtools::build_rmd("vignettes/run_on_demo_repo.Rmd")
+# browseURL("vignettes/run_on_demo_repo.html")
+#
+#
+# # increase version
 pkgdown::build_site()
+
+
 
 #rhub::check_for_cran()
 usethis::use_version()
