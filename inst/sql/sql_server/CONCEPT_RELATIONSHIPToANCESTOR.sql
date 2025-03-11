a43407bc3ace9a086a69bb222ab3785d01ddc9c9
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-03-11 12:38:21 +0200

    added CONCEPT_ANCESTOR table from CONCEPT_RELATIONSHIP code and test

---
+++ inst/sql/sql_server/CONCEPT_RELATIONSHIPToANCESTOR.sql
@@ -0,0 +1,57 @@
+-- DESCRIPTION:
+-- Adds non-standard FinOMOP and specified vocabularies concept ancestry to the concetp_ancestor table
+--
+-- PARAMETERS:
+--
+-- - vocabularyDatabaseSchema: schema containing the vocabulary and STCM tables
+-- - vocabularyList: comma-separated list of vocabulary_ids to include
+
+-- 1-  Create a temporary table with concept relationships that have
+-- 1-1 Any concept > 2 billion or from specified vocabularies
+-- 1-2 Only get that have relationship `Subsumes` present
+
+DROP TABLE IF EXISTS #relationships;
+
+SELECT cr.concept_id_1, cr.concept_id_2
+INTO #relationships
+FROM @vocabularyDatabaseSchema.concept c
+INNER JOIN @vocabularyDatabaseSchema.concept_relationship cr
+  ON cr.concept_id_1 = c.concept_id 
+WHERE c.vocabulary_id IN (@vocabularyList)
+  AND cr.relationship_id = 'Subsumes'
+ORDER BY cr.concept_id_1, cr.concept_id_2;
+
+-- 2- Insert the created non-standard concept ancestries to concept_ancestor table in omop vocab
+INSERT INTO @vocabularyDatabaseSchema.concept_ancestor
+(
+	ancestor_concept_id,
+	descendant_concept_id,
+	min_levels_of_separation,
+	max_levels_of_separation
+)
+-- 3- Create a min and max distance as 1 for every concept relation
+-- 3-1 Recursively check descendant_concept_id until there are no descendants
+WITH RECURSIVE ancestor_cte AS (
+-- Base case: direct relationships
+	SELECT concept_id_1 AS ancestor_concept_id,
+			concept_id_2 AS descendant_concept_id,
+			1 AS min_levels_of_separation,
+			1 AS max_levels_of_separation
+	FROM #relationships
+	
+	UNION ALL
+	
+-- Recursive case: find descendant relationships
+	SELECT r.concept_id_1 AS ancestor_concept_id,
+			c.descendant_concept_id AS descendant_concept_id,
+			c.min_levels_of_separation + 1 AS min_levels_of_separation,
+			c.max_levels_of_separation + 1 AS max_levels_of_separation
+	FROM #relationships r
+	JOIN ancestor_cte c
+	ON r.concept_id_2 = c.ancestor_concept_id
+)
+SELECT *
+FROM ancestor_cte;
+
+-- 4- Remove the temporary table
+DROP TABLE #relationships;
